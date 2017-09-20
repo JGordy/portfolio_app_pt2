60c001d9461e26bedb1a2dbd16f9dc7e0787920a
Joseph Gordy <jgordy2424@gmail.com>
2017-09-20 17:30:04 -0400

    finished with styling on the About page

--- src/components/About.js
+++ src/components/About.js
@@ -10,13 +10,15 @@ export default class About extends Component {
           <img className="fitz" src={require("../images/Fitz.jpg")}/>
           <div className="event_info">
             <h3>Meet Fitz</h3>
+            <h5 className="date">9/17/17</h5>
             <p>This is the new addition to the family. My wife and I rescued him from a shelter in Alpharetta. Fitz is a two month old hound/pointer mix who loves sticks larger than he is, loves to cuddle on the couch, and take walks with us by the river. He is especially quiet for a hound, which are known for thier howling.</p>
           </div>
         </div>
         <div className="event_reverse">
           <div className="event_info">
             <h3>Apple picking</h3>
-            <p></p>
+            <h5 className="date">8/27/17</h5>
+            <p>Road trips are always great! This one was to north Georgia at Merciers Orchard. After picking our own apples in the orchard we settled  down for a fantastic lunch, and topped it off with a fudge covered granny smith apple. Yum!</p>
           </div>
             <img className="apples" src={require("../images/apple_picking.jpg")}/>
 
@@ -25,13 +27,15 @@ export default class About extends Component {
           <img className="balloon" src={require("../images/hot_air_balloon.jpg")}/>
           <div className="event_info">
             <h3>Hot Air Balloon ride</h3>
-            <p></p>
+            <h5 className="date">9/1/17</h5>
+            <p>First hot air balloon ride for both my wife and I at Calloway Gardens. We only got about 40ft in the air, but the views were still great.</p>
           </div>
         </div>
         <div className="event_reverse">
           <div className="event_info">
             <h3>Watching a movie</h3>
-            <p></p>
+            <h5 className="date">8/26/17</h5>
+            <p>This was a fun night at campus. A movie scene from Ant Man 2 was being filmed on the bottom floor of my school building. Sadly, I didn't see Paul Rudd or Evangeline Lily. :(</p>
           </div>
           <img className="movie" src={require("../images/movie_set.jpg")}/>
         </div>
